cf72f869faf3edc46a0560ad593418fa53b03582
Miguel Anguita <miguel.anguita@soprasteria.com>
2018-02-20 09:11:32 +0100

    Default methods example
    Co-Authored-By: milfist <milfist@users.noreply.github.com>

--- 1_02_default_methods/src/org/formacion/Agrupador.java
+++ 1_02_default_methods/src/org/formacion/Agrupador.java
@@ -1,11 +1,18 @@
 package org.formacion;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 public interface Agrupador {
 
-	void add (Object elemento);
-	
+	void add(Object elemento);
+
 	int numeroElementos();
-	
+
+	default void addAll(Collection<Object> c) {
+		for (Iterator<Object> iterator = c.iterator(); iterator.hasNext();) {
+			this.add(iterator.next());
+		}
+	}
+
 }
